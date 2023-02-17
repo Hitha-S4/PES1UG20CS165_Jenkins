3bd01a7257eba147526ee60bf181799f9d0d5ff4
Hitha-S-PESU <125564749+Hitha-S-PESU@users.noreply.github.com>
2023-02-17 10:53:17 +0530

    C3

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I,Hitha have successfully built and run\n";
+  cout << "I,Hitha PES1UG20CS165 have successfully built and run\n";
   return 0;
 }
